de3f81dbdc38d1515287757eb180525d516bef7a
Faiyaz69 <faiyaz@eng.rizvi.edu.in>
2024-10-16 10:29:58 +0530

    Create task 01.c

---
+++ Exp 01/task 01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("NAME\t\t:\tFAIYAZ SHAH\n");
+    printf("AGE\t\t:\t20\n");
+    printf("CLASS\t\t:\tFE\n");
+    printf("DIVISION\t:\tE\n");
+    printf("UIN\t\t:\t2471S044");
+    return 0;
+}
