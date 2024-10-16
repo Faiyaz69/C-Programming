d164d998eb74da2fada120f6f1dd867cfb8e3ec9
Faiyaz69 <faiyaz@eng.rizvi.edu.in>
2024-10-16 10:11:56 +0530

    Create task 01.c

---
+++ task 01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\t\t\t***Even Odd Finder***\n\n\n");
+printf("Enter Number :");
+scanf("%d", &num);
+
+if(num%2==0){printf("%d is even",num);
+}
+
+else{printf("%d is odd", num);
+}
+return 0;
+
+}
